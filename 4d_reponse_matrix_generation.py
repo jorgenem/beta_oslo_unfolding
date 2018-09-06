Corrected CalcResponse() and normalization of Resp
- Corrected the adding of gamma-rays in CalcResponse(): no double counting
- Account for efficiency

4d_reponse_matrix_generation.py
@@ -4,27 +4,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 
-fname_resp = 'resp-sun2015.dat'
-fname_resp_mat = 'response_matrix-sun2015.m'
+fname_resp = 'resp-SuN2015-20keV-1p0FWHM.dat'
+fname_resp_mat = 'response_matrix-SuN2015-20keV-1p0FWHM.dat'
 R_2D, cal_resp, E_resp_array, tmp = read_mama_2D(fname_resp_mat)
 # R_2D = div0(R_2D , R_2D.sum(rebin_axis=1))
 
-# Assumed lower threshold for gammas in response matrix
-E_thres = 100
-i_thres = np.argmin(np.abs(E_resp_array - E_thres))
-R_2D[:,:i_thres] = 0
-for i in range(R_2D.shape[0]):
-	try:
-		R_2D[i,:] = R_2D[i,:] / R_2D[i,:].sum()
-	except:
-		R_2D[i,:] = 0
-
-
-# f_cmp, ax_cmp = plt.subplots(1,1)
-# ax_cmp.plot(E_resp_array, R_2D[400,:])
-
-
-
+# Read efficiency and other 1-D response variables:
 resp = []
 with open(fname_resp) as file:
     # Read line by line as there is crazyness in the file format
@@ -35,6 +20,28 @@ with open(fname_resp) as file:
             resp.append(row)
         except:
             break
+
+resp = np.array(resp)
+# Name the columns for ease of reading
+FWHM = resp[:,1]
+eff = resp[:,2]
+pf = resp[:,3]
+pc = resp[:,4]
+ps = resp[:,5]
+pd = resp[:,6]
+pa = resp[:,7]
+
+# Assumed lower threshold for gammas in response matrix
+E_thres = 100
+i_thres = np.argmin(np.abs(E_resp_array - E_thres))
+R_2D[:,:i_thres] = 0
+
+for i in range(R_2D.shape[0]):
+	norm = R_2D[i,:].sum()
+	if(norm>0):
+		R_2D[i,:] = R_2D[i,:] / norm #* eff[i]
+	else:
+		R_2D[i,:] = 0
 
 
 
@@ -55,7 +62,16 @@ def CalcResponse(E1s, E2s=None, E3s=None, E4s=None, E_resp_array=None, N_resp_dr
 
 	def FoldEg(Eg, Eg_arr=E_resp_array, size=N_resp_draws, response=response):
 		index_Eg = np.argmin(np.abs(Eg_arr - Eg))
-		Eg_folded = np.random.choice(Eg_arr, size=size, p=response[index_Eg,:])
+		# choosing rand accounts for the efficiency; As the efficiency read from file currently
+		# does not always correspons with the counts in R_2D, see #3, we need two if tests
+		if R_2D[index_Eg,:].sum() > 0:
+			rand = np.random.uniform()
+			if rand <= eff[index_Eg]:
+				Eg_folded = np.random.choice(Eg_arr, size=size, p=response[index_Eg,:])
+			else: 
+				Eg_folded=np.nan # Give Energy 0 to events that are not recorded.
+		else: 
+			Eg_folded = np.nan # Give Energy 0 to events that are not recorded.
 		return Eg_folded
 
 	print("working on response matrix")
@@ -70,15 +86,16 @@ def CalcResponse(E1s, E2s=None, E3s=None, E4s=None, E_resp_array=None, N_resp_dr
 		# print(Eg_folded_arr)
 
 		# Ex calculated as the sum over Egs
-		Ex_folded = np.sum(Eg_folded_arr,axis=0)
+		Ex_folded = np.sum(np.nan_to_num(Eg_folded_arr),axis=0)
 		# print(Ex_foldZed)
 
 		# fill the matrix
 		for i_resp_draws in range(N_resp_draws):
 			i_Ex = np.argmin(np.abs(E_resp_array - Ex_folded[i_resp_draws]))
-			for Eg in Eg_folded_arr:
-				i_Eg = np.argmin(np.abs(E_resp_array - Eg[i_resp_draws]))
-				matrix[i_Ex,i_Eg] += 1
+			for Eg in Eg_folded_arr[:,i_resp_draws]:
+				if np.isfinite(Eg):
+					i_Eg = np.argmin(np.abs(E_resp_array - Eg))
+					matrix[i_Ex,i_Eg] += 1
 	print ("Finished repsonse matrix")
 	return matrix
 