908f48e9b951ad799b62b1d66b282532b10b6333
Jørgen Eriksson Midtbø <jorgenem@gmail.com>
2018-09-10 18:02:02 +0200

    Folding with multiplicity now works!

--- fold-gamma_energy_axes-random_multiplicity.py
+++ fold-gamma_energy_axes-random_multiplicity.py
@@ -46,6 +46,15 @@ eff = resp[:,2]
 # pd = resp[:,6]
 # pa = resp[:,7]
 
+
+
+Emax = 2000 # Crop response matrix (and thus also event generation) to efficiate computation
+i_Emax = np.argmin(np.abs(E_resp_array-Emax))
+R_2D = R_2D[0:i_Emax,0:i_Emax]
+E_resp_array = E_resp_array[0:i_Emax]
+
+
+
 # Assumed lower threshold for gammas in response matrix
 E_thres = 100
 i_thres = np.argmin(np.abs(E_resp_array - E_thres))
@@ -60,10 +69,6 @@ for i in range(R_2D.shape[0]):
 
 
 
-Emax = 2000 # Crop response matrix (and thus also event generation) to efficiate computation
-i_Emax = np.argmin(np.abs(E_resp_array-Emax))
-R_2D = R_2D[0:i_Emax,0:i_Emax]
-E_resp_array = E_resp_array[0:i_Emax]
 
 
 # === Generate events ===
@@ -82,172 +87,107 @@ except:
 
 
 
-print("events_t =", events_t)
+print("Events true:", flush=True)
+print(events_t, flush=True)
 
 
 
 
 # === Fold them with detector response and pileup: ===
-def FoldEg(Egs, Mf_max, Eg_arr, response, pileup=True):
+def FoldEg(Egs_t, Mf_max, Eg_arr, response, pileup=True):
 	"""
 	This function folds all Egs from a single event.
 	"""
 
 	
-	print("Folding. True gammas =", Egs, flush=True)
-	
+	# print("Folding. True gammas =", Egs_t, flush=True)
+	Mt_curr = len(Egs_t[Egs_t>0]) # Number of true gammas
 
-	if pileup and Mt_curr > Mf_curr:
-		# First decide the detector multiplicity for this event. It's bounded upwards by Mf (4 in case of SuN)
-		# A first approx. is a linearly increasing probability from 0 to true multiplicity for current event, 
-		# but not higher than Mf_max.
-		print("Deciding multiplicity", flush=True)
-		Mt_curr = len(Egs[Egs>0])
-		Mf_curr = int(min(Mf_max, np.random.triangular(1,Mt_curr,Mt_curr))) if Mt_curr > 1 else 1 # Don't make any events with multiplicity zero. We can model the detector efficiency elsewhere.
-		print("Mt_curr =", Mt_curr, "Mf_curr =", Mf_curr, flush=True)
+	if pileup: # Is pileup desired?
+		# For each true gamma, assign it to pileup with probability p_pile:
+		p_pile = 0.2
+		indices_pile = [] # Store indices in Egs_true
+		counter_nopile = 0
+		map_to_pileup = {} # Map the indices that are *not* pileup to a new index set so that there are no index holes
+						# For example if Mt = 3 and index 1 is pileup, then map_nopile = {0:0, 2:1}.
+						# Then we decide which of the new indices to add each pileup event to.
+		for i_t in range(Mt_curr):
+			# Make sure at least one gamma is not assigned to pileup:
+			if i_t == Mt_curr-1 and counter_nopile == 0:
+				map_to_pileup[i_t] = counter_nopile
+				counter_nopile += 1
+				break
 
-		# Decide which true gammas come in the same detector.
-		# Do this by drawing random pairs of indices 
-		Mf_tmp = Mt_curr
-		indices_combined = {} # Create a dictionary of indices stored as key, value pairs to make mapping easy
-		indices_used = []
-		print("Deciding gamma combinations", flush=True)
-		while Mf_tmp > Mf_curr:
-			indices_combined_curr = [0,0]
-			# Make sure we combine two different gammas, and that they have not already been combined.
-			while indices_combined_curr[0] == indices_combined_curr[1] or indices_combined_curr[0] in indices_used or indices_combined_curr[1] in indices_used:
-				indices_combined_curr = np.random.randint(0,Mt_curr+1),np.random.randint(0,Mt_curr+1)
-				# Make sure key is lower than value, so that we map both to the lowest index 
-				# (it's just easier to code below):
-				if indices_combined_curr[0] > indices_combined_curr[1]:
-					indices_combined_curr = [indices_combined_curr[1],indices_combined_curr[0]]
-			indices_combined[indices_combined_curr[0]] = indices_combined_curr[1]
-			Mf_tmp -= 1
-
-		# Combine gammas:
-		print("Piling gammas", flush=True)
-		Egs_piled = np.zeros(Mf_curr)
-		i_piled = 0
-		indices_used = []
-		while len(indices_used) < Mt_curr:
-			if not i_piled in indices_used:
-				try: # If index is one of the key, value pairs:
-					i1 = i_piled
-					i2 = indices_combined[i_piled]
-					print("i_piled =", i_piled, "i1 =", i1, "i2 =", i2, flush=True)
-					Egs_piled[i_piled] = Egs[i1] + Egs[i2]
-					indices_used.append(i1)
-					indices_used.append(i2)
-					i_piled += 1
-				except KeyError: # Not one of the key, value pairs
-					print("i_piled =", i_piled, flush=True)
-					Egs_piled[i_piled] = Egs[i_piled]
-					indices_used.append(i_piled)
-					i_piled += 1
+			r_pile = np.random.uniform()
+			if r_pile < p_pile:
+				# This gamma should be piled.
+				indices_pile.append(i_t)
 			else:
-				i_piled += 1
-			print("Egs_piled =", Egs_piled, flush=True)
+				# This gamma should not
+				map_to_pileup[i_t] = counter_nopile
+				counter_nopile += 1
+
+		# Calculate multiplicity of current event after pileup:
+		Mf_curr = Mt_curr - len(indices_pile)
+
+		# Decide mapping of pileup gammas:
+		for i_t in indices_pile:
+			map_to_pileup[i_t] = np.random.randint(0,Mf_curr) if Mf_curr > 0 else 0
+
+		Egs_piled = np.zeros(Mf_curr)
+		for i_t in range(Mt_curr):
+			Egs_piled[map_to_pileup[i_t]] += Egs_t[i_t]
+
 	else:
-		# Do not model pileup:
+		# Do not include pileup:
 		Egs_piled = Egs
 
+	# print("Piled gammas =", Egs_piled, flush=True)
 
 
-	
-	return Egs_piled
+	# Now proceed to fold each gamma with detector response:
+	Egs_folded = np.zeros(Mf_curr)
+	for i in range(Mf_curr):
+		Eg = Egs_piled[i]
+		index_Eg = np.argmin(np.abs(Eg_arr - Eg))
+		if R_2D[index_Eg,:].sum() > 0:
+			# choosing rand accounts for the efficiency; As the efficiency read from file currently
+			# does not always correspons with the counts in R_2D, see #3, we need two if tests
+			rand = np.random.uniform()
+			if rand <= eff[index_Eg]:
+				# If the gamma is not lost to efficiency, redistribute its energy somewhere in the response:
+				Eg_folded = np.random.choice(Eg_arr, p=response[index_Eg,:])
+			else: 
+				Eg_folded = 0 # Give Energy 0 to events that are not recorded.
+		else: 
+			Eg_folded = 0 # Give Energy 0 to events that are not recorded (below detector threshold)
 
+		Egs_folded[i] = Eg_folded
 
-	# for i_Eg in range(Mt_curr)
-	# if R_2D[index_Eg,:].sum() > 0:
-	# index_Eg = np.argmin(np.abs(Eg_arr - Eg))
-	# # choosing rand accounts for the efficiency; As the efficiency read from file currently
-	# # does not always correspons with the counts in R_2D, see #3, we need two if tests
-	# 	rand = np.random.uniform()
-	# 	if rand <= eff[index_Eg]:
-	# 		Eg_folded = np.random.choice(Eg_arr, size=size, p=response[index_Eg,:])
-	# 	else: 
-	# 		Eg_folded = 0 # Give Energy 0 to events that are not recorded.
-	# else: 
-	# 	Eg_folded = 0 # Give Energy 0 to events that are not recorded (below detector threshold)
-	# return Egs_folded
+	return Egs_folded
 
 
 Mf_max = 4 # Max detector multiplicity
 
 
+fname_ev_f = "generated_events-folded-Emax2MeV-{:d}_events.npy".format(N_events)
+try:
+	events_f = np.load(fname_ev_f)
+except:
+	events_f = np.zeros((N_events, Mf_max))
+	for i_ev in range(N_events):
+		Egs_folded = FoldEg(events_t[i_ev,:], Mf_max=4, Eg_arr=E_resp_array, response=R_2D)
+		events_f[i_ev,0:len(Egs_folded)] = Egs_folded
+	np.save(fname_ev_f,	events_f)
 
-for i_ev in range(N_events):
-	Egs_folded = FoldEg(events_t[i_ev,:], Mf_max=4, Eg_arr=E_resp_array, response=R_2D)
-	print("Egs_folded[{:d}] =".format(i_ev), Egs_folded, flush=True)
+print("Events folded:", flush=True)
+print(events_f)
 
 
 sys.exit(0)
 
 
 
-
-
-
-# plt.pcolormesh(E_resp_array, E_resp_array, R_2D, norm=LogNorm())
-# sys.exit(0)
-
-def CalcResponseEgaxes(E1s, E2s=None, E3s=None, E4s=None, E_resp_array=None, N_resp_draws=None, response=None):
-	# find the multiplicity
-	M = 1
-	if E2s is not None: M = 2
-	if E3s is not None: M = 3
-	if E4s is not None: M = 4
-
-	Nbins = len(E_resp_array)
-	matrix = np.zeros(tuple([Nbins for i in range(M)])) # Make an M-dimensional array
-	indices_E_resp_array = np.linspace(0,Nbins-1, Nbins).astype(int)
-
-	def FoldEg(Eg, Eg_arr=E_resp_array, size=N_resp_draws, response=response):
-		index_Eg = np.argmin(np.abs(Eg_arr - Eg))
-		# choosing rand accounts for the efficiency; As the efficiency read from file currently
-		# does not always correspons with the counts in R_2D, see #3, we need two if tests
-		if R_2D[index_Eg,:].sum() > 0:
-			rand = np.random.uniform()
-			if rand <= eff[index_Eg]:
-				Eg_folded = np.random.choice(Eg_arr, size=size, p=response[index_Eg,:])
-			else: 
-				Eg_folded=np.nan # Give Energy 0 to events that are not recorded.
-		else: 
-			Eg_folded = np.nan # Give Energy 0 to events that are not recorded.
-		return Eg_folded
-
-	print("working on response matrix")
-
-	# find the response and Ex for each gamma ray in N_draws
-	for i_draw in range(N_draws):
-		Eg_folded_arr = np.zeros((M,N_resp_draws))
-		Eg_folded_arr[0,:] = FoldEg(E1s[i_draw])
-		if E2s is not None: Eg_folded_arr[1,:] = FoldEg(E2s[i_draw])
-		if E3s is not None: Eg_folded_arr[2,:] = FoldEg(E3s[i_draw])
-		if E4s is not None: Eg_folded_arr[3,:] = FoldEg(E4s[i_draw])
-		# print(Eg_folded_arr)
-
-		# Ex calculated as the sum over Egs
-		# Ex_folded = np.sum(np.nan_to_num(Eg_folded_arr),axis=0)
-		# print(Ex_foldZed)
-
-		# fill the matrix
-		for i_resp_draws in range(N_resp_draws):
-			# i_Ex = np.argmin(np.abs(E_resp_array - Ex_folded[i_resp_draws]))
-			Egs = np.nan_to_num(Eg_folded_arr[:,i_resp_draws])
-			# Egs[::-1].sort()
-			if M == 2:
-				i_Eg1 = np.argmin(np.abs(E_resp_array - Egs[0]))
-				i_Eg2 = np.argmin(np.abs(E_resp_array - Egs[1]))
-				matrix[i_Eg1, i_Eg2] += 1
-			# print("Egs =", Egs, flush=True) 
-			# indices_Eg = np.argmin(np.abs(np.tile(E_resp_array,M).reshape(Nbins,M) - Egs), axis=0)
-			# print("indices_Eg =", indices_Eg, flush=True)
-			# matrix[tuple(indices_Eg)] += 1
-	print ("Finished repsonse matrix")
-	return matrix
-
-Emax = 10*1e3
 
 
 
